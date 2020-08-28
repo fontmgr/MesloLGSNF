0235a576ed807d845463cdedbc4abb7588d5b9c3
Jason <jason@casjay.net>
2020-08-28 17:47:58 -0400

    🦈🏠🐜❗ Updated installer ❗🐜🦈🏠

--- install.sh
+++ install.sh
@@ -35,10 +35,6 @@ fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 system_installdirs
 
-# OS Support: supported_os unsupported_oses
-
-unsupported_oses
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Make sure the scripts repo is installed
@@ -62,62 +58,18 @@ fontmgr_install
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Version
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Script options IE: --help
 
 show_optvars "$@"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Do not update
-
-#systemmgr_noupdate
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Requires root - no point in continuing
-
-#sudoreq  # sudo required
-#sudorun  # sudo optional
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # end with a space
 
 APP=""
-PERL=""
-PYTH=""
-PIPS=""
-CPAN=""
-GEMS=""
 
 # install packages - useful for package that have the same name on all oses
 install_packages $APP
-
-# install required packages using file
-install_required $APP
-
-# check for perl modules and install using system package manager
-install_perl $PERL
-
-# check for python modules and install using system package manager
-install_python $PYTH
-
-# check for pip binaries and install using python package manager
-install_pip $PIPS
-
-# check for cpan binaries and install using perl package manager
-install_cpan $CPAN
-
-# check for ruby binaries and install using ruby package manager
-install_gem $GEMS
-
-# Other dependencies
-dotfilesreq git
-dotfilesreqadmin
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -132,13 +84,13 @@ ensure_perms
 
 if [ -d "$APPDIR/.git" ]; then
   execute \
-  "git_update $APPDIR" \
-  "Updating $APPNAME configurations"
+    "git_update $APPDIR" \
+    "Updating $APPNAME configurations"
 else
   execute \
-  "backupapp && \
+    "backupapp && \
         git_clone -q $REPO/$APPNAME $APPDIR" \
-  "Installing $APPNAME configurations"
+    "Installing $APPNAME configurations"
 fi
 
 # exit on fail
@@ -153,8 +105,8 @@ run_postinst() {
 }
 
 execute \
-"run_postinst" \
-"Running post install scripts"
+  "run_postinst" \
+  "Running post install scripts"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
